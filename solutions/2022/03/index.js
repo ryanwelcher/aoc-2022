2fe933c51c7863ff206ba785499183f1d7b4b334
Ryan Welcher <me@ryanwelcher.com>
2022-12-08 16:46:50 -0500

    Day 3 part 1

---
+++ solutions/2022/03/index.js
@@ -0,0 +1,41 @@
+export const parseInput = (input) => input.split('\n')
+const alphabet = [
+  'a','b','c','d', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z',
+  'A','B','C','D','E','F','G','H','I','J','K','L','M','N','O','P','Q','R','S','T','U','V','W','X','Y','Z'
+];
+
+const divideIntoCompartments = (input) => {
+  return [
+    input.slice(0, input.length / 2).split(''),
+    input.slice(input.length / 2, input.length).split('')
+  ]
+}
+
+const tallyPriorities = (priorities) => {
+  let tally = 0;
+  for (let i = 0; i < priorities.length; i++) {
+    tally += Number(alphabet.indexOf(priorities[i])) + 1;
+  }
+
+  return tally;
+}
+
+
+export const part1 = (input) => {
+  const priorities = [];
+  // loop through all rucksacks
+  for (let i = 0; i < input.length; i++) {
+    const [compOne, comp2] = divideIntoCompartments(input[i]);
+    for (let i = 0; i < compOne.length; i++) {
+      if ( comp2.includes( compOne[i] ) ) {
+        priorities.push(compOne[i])
+        break;
+      }
+    }
+  }
+  return tallyPriorities(priorities)
+}
+
+export const part2 = (input) => {
+  return
+}
